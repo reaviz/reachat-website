7df6e3dfd35ef26b34a3e3329fa1e46ccbd9c7ec
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-08-01 21:35:59 +0300

    update reachat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/reablocks/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/reachat/**/*.{js,jsx,ts,tsx}",
     "./theme.config.tsx",
   ],
   theme: {
